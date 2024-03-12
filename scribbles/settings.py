c271cc8a2a30a144ceadb1d21d483c5dbb5ff601
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-12 11:56:36 +0000

    Install pyscopg2 and link up database in settings

--- scribbles/settings.py
+++ scribbles/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +78,15 @@ WSGI_APPLICATION = 'scribbles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("postgres://pdjbljge:M3GpKhy2rwmCtXDO4-4Tkh3rSSsiIiIz@flora.db.elephantsql.com/pdjbljge"))
 }
 
 
