e16750ed98c92709a39cd79e0d3a87d56761fa94
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-12 14:57:20 +0000

    populate blog urls with post list

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
+class PostList(generic.ListView):
+    model = Post
 
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
