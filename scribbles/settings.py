fa3ded2865b1f3e2702aa842e2d6a8cf3665e62f
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-12 11:53:29 +0000

    Create env.py

--- scribbles/settings.py
+++ scribbles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)wh*i&k%cn1ut54_ulvvn*+g(kyx(56p1s*+e346d=grb+ptdi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-katiejanecoughl-journal-cjt6iql6cb5.ws-eu108.gitpod.io', '.herokuapp.com']
 
