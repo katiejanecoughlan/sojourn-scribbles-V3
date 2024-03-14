689dfda5d0d7b4228cffc544601e54919b8e2381
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-14 15:47:20 +0000

    Add collab form

--- about/models.py
+++ about/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
@@ -9,3 +10,13 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
