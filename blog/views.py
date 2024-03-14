e028dece547df662c3a37bc227d9841871dbb01b
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-14 15:27:24 +0000

    Handle post request from comment form

--- blog/views.py
+++ blog/views.py
@@ -29,6 +29,15 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
     comment_form = CommentForm()
 
 
