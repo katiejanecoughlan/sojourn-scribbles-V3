9856d367661b747a0a737eae40d9415226ffbb7a
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-14 15:31:04 +0000

    Display confirmation msg on comment box

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -37,6 +38,10 @@ def post_detail(request, slug):
             comment.author = request.user
             comment.post = post
             comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
 
     comment_form = CommentForm()
 
