fef9cfe59ee583e92d8eea293c92cd2469db5398
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-14 19:14:34 +0000

    Add view, url and btn to edit a comment

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
 ]
