40ef2c98b5a30c25d0f5f3bccd3b4884a7aa49e1
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-15 12:53:20 +0000

    Add docstrings to blog admin app and forms

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    """
+    Form class for users to comment on a post
+    """
     class Meta:
         model = Comment
         fields = ('body',)
