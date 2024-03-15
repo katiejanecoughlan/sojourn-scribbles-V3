8477f989f356c38089d9c79fb9bfe0b3f04f68d2
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-15 19:01:50 +0000

    Add author row to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
@@ -7,10 +8,13 @@ class Profiles(models.Model):
     """
     Stores a single about me text
     """
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="profile_bio"
+    )
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-
+    
     def __str__(self):
         return self.title
