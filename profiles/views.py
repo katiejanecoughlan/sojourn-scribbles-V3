803a93ef6acf6a06b918f040cf0d401c22e38ccd
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-22 20:44:12 +0000

    Rick Roll Footer links

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Profiles
+from .forms import ProfileForm
 from blog.forms import PostForm
 
 @login_required
