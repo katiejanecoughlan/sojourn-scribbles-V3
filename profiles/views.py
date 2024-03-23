45e9be67a4b3fc1541f6dc51523508b1915ad727
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-23 14:54:25 +0000

    Reorder profile view if statement

--- profiles/views.py
+++ profiles/views.py
@@ -18,27 +18,34 @@ def profiles_me(request):
     if request.method == 'POST':
         post_form = PostForm(request.POST, request.FILES)
         profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
+
         if post_form.is_valid():
             post = post_form.save(commit=False)
             post.author = request.user
             post.save()  # This will automatically handle the featured_image upload to Cloudinary
             messages.success(request, 'Post created successfully.')
             return redirect('home')
+        
         elif profile_form.is_valid():
             profile_instance = profile_form.save(commit=False)
             profile_instance.user = request.user
             profile_instance.save()
-            messages.success(request, 'Profile updated successfully.')
+            # messages.success(request, 'Profile updated successfully.') #Disabled due to bug
 
             # Fetch the updated profile instance
             updated_profile = Profiles.objects.get(pk=profile_instance.pk)
 
             return render(request, "profiles/profiles.html", {"profile_form": profile_form, "post_form": post_form, "profiles": updated_profile})
+
         else:
+            # If neither form is valid, show an error message
             messages.error(request, 'Error creating post or updating profile.')
     else:
+        # If not a POST request, initialize forms without any submitted data
         post_form = PostForm()
         profile_form = ProfileForm(instance=profile)
 
     return render(request, "profiles/profiles.html", {"profile_form": profile_form, "post_form": post_form, "profiles": profile})
+
+
 
