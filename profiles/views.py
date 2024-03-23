d455d64fad3e4277e638e3dea73f1cc20b499d31
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-23 11:22:24 +0000

    Update redirect urls on profiles view to resolve integrity error

--- profiles/views.py
+++ profiles/views.py
@@ -7,11 +7,15 @@ from blog.forms import PostForm
 
 @login_required
 def profiles_me(request):
-    profiles = Profiles.objects.filter(user=request.user).first()  # Use filter() instead of get()
+    profiles_queryset = Profiles.objects.filter(user=request.user)
+    if profiles_queryset.exists():
+        profile = profiles_queryset.first()
+    else:
+        profile = None
 
     if request.method == 'POST':
         post_form = PostForm(request.POST, request.FILES)
-        profile_form = ProfileForm(request.POST, request.FILES)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=profile)
         if post_form.is_valid():
             post = post_form.save(commit=False)
             post.author = request.user
@@ -19,18 +23,18 @@ def profiles_me(request):
             messages.success(request, 'Post created successfully.')
             return redirect('home')
         elif profile_form.is_valid():
-            # Process profile_form data
             profile_instance = profile_form.save(commit=False)
             profile_instance.user = request.user
             profile_instance.save()
             messages.success(request, 'Profile updated successfully.')
-            return redirect('profiles_me')
+            return redirect('profiles')
         else:
-            messages.error(request, 'Error creating post.')
+            messages.error(request, 'Error creating post or updating profile.')
     else:
         post_form = PostForm()
-        profile_form = ProfileForm()
+        profile_form = ProfileForm(instance=profile)
 
-    return render(request, "profiles/profiles.html", {"profiles": profiles, "post_form": post_form, "profile_form": profile_form})
+    return render(request, "profiles/profiles.html", {"profile_form": profile_form, "post_form": post_form})
+
 
 
