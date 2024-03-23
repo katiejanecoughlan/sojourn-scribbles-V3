f2989722fbbfbf27a4fa689192ff771bb3f747c3
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-23 13:25:59 +0000

    Updat cloundinary handling in profile model and make migrations

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profiles(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=200)
-    profile_image = CloudinaryField('image', default='placeholder')
+    profile_image = CloudinaryField(default='placeholder', folder='profile/', null=False, blank=False)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
