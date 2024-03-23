66d773d846ba0260adc4e50cf42c7aa6d51a0317
Katie Coughlan <katiejanecoughlan@gmail.com>
2024-03-23 14:18:40 +0000

    Import django core validators for profile model and migrate

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxLengthValidator
 from cloudinary.models import CloudinaryField
 
 class Profiles(models.Model):
@@ -11,7 +12,7 @@ class Profiles(models.Model):
     title = models.CharField(max_length=200)
     profile_image = CloudinaryField(default='placeholder', folder='profile/', null=False, blank=False)
     updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = models.TextField(validators=[MaxLengthValidator(2000)])
 
     def __str__(self):
         return self.title
